d2cd6e39487cbacf6a38d24da3162187827f533d
yeren0914 <429441861@qq.com>
2025-11-04 14:25:15 +0800

    fix: bridge css change add Recipient

--- src/pages/bridge/bridge.tsx
+++ src/pages/bridge/bridge.tsx
@@ -1,8 +1,7 @@
 import { useMemo, useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
-import { Button, Popup, SearchBar, Tabs } from 'antd-mobile'
-import { DownOutline } from 'antd-mobile-icons'
+import { Button, Popup, Tabs } from 'antd-mobile'
 import { useLocation } from 'react-router-dom'
 
 
@@ -13,7 +12,7 @@ import NoDataDom from "@/components/NoDataDom";
 import NumberInput from '@/components/NumberInput';
 import TokenImg from "@/components/TokenImg";
 import { useClipboard } from '@/components/useClipboard';
-import store, { RootState } from '@/store';
+import { RootState } from '@/store';
 
 import { formatAddress } from '@/utils/util'
 
@@ -23,7 +22,7 @@ import { Keyring } from '@/chrome/keyring'
 import { Contact } from '@/chrome/contact'
 import { Account } from '@/chrome/account'
 import { Evm } from '@/chrome/evm'
-import {EvmTokenList, EvmNetwork} from "@/model/evm";
+import { EvmTokenList, EvmNetwork } from "@/model/evm";
 import { ethers } from "ethers";
 
 interface TokenItem {
@@ -46,7 +45,7 @@ interface SwitchItem {
     network: string
 }
 
-type PopupType = "FORM"  | "TO" | ""
+type PopupType = "FORM" | "TO" | ""
 
 const Bridge = () => {
     const { state } = useLocation()
@@ -54,9 +53,6 @@ const Bridge = () => {
 
     const { preference } = useSelector((state: RootState) => state.preference);
     const [network] = useState<EvmNetwork>(state?.network)
-
-
-    const { handleCopy } = useClipboard();
 
     const [swapLoading, setSwapLoading] = useState(false)
     const [popupVisible, setPopupVisible] = useState(false)
@@ -67,28 +63,7 @@ const Bridge = () => {
     const [accountsValue, setAccountsValue] = useState<AccountsSubListDisplay[] | null>(null)
     const [contactValue, setContactValue] = useState<Address[] | null>(null)
     const [address, setAddress] = useState("")
-
-    // const [selectType, setSelectType] = useState<PopupType>('FORM')
-    
-    // const [bridgeData, setBridgeData] = useState({
-    //     from: {
-    //         address: preference.currentAccount?.address,
-    //         token: "KAS",
-    //         balance: preference.currentAccount?.balance || "0",
-    //         amount: 0,
-    //         desc: 8,
-    //         isKaspa: true
-    //     },
-    //     to: {
-    //         address: preference.currentAccount?.ethAddress,
-    //         token: 'KAS',
-    //         balance: 3333,
-    //         amount: 3333,
-    //         desc: 4,
-    //         isKaspa: false,
-    //         network: network
-    //     },
-    // })
+    const [l1Address, setL1Address] = useState("")
 
     const [fromData, setFromData] = useState<SwitchItem>({
         address: preference.currentAccount?.address!,
@@ -110,7 +85,7 @@ const Bridge = () => {
         network: ""
     })
 
-    const syncBalance = async () =>  {
+    const syncBalance = async () => {
         getBalance(fromData.address).then(r => {
             if (fromData.balance != r) {
                 fromData.balance = r
@@ -127,7 +102,7 @@ const Bridge = () => {
         }
     }
 
-    const getBalance = async (data: any) =>  {
+    const getBalance = async (data: any) => {
         if (data.isKaspa) {
             return await Account.getBalance(data.from.address)
         } else {
@@ -160,7 +135,7 @@ const Bridge = () => {
             desc: 4,
         },
     })
-    
+
     const submitDisabled = useMemo(() => {
         return !bridgeData.from.amount || !bridgeData.to.address || !amount || !toAmount
     }, [bridgeData, amount, toAmount])
@@ -188,16 +163,6 @@ const Bridge = () => {
             default:
                 break;
         }
-    }
-
-    const showPopup = (type:PopupType, item: TokenItem) => {
-        if(item.token === "KAS") return
-        // setSelectType(type)
-        // setTokenVisible(true)
-    }
-    const hidePopup = () => {
-        // setSelectType("")
-        // setTokenVisible(false)
     }
 
     const switchInfo = () => {
@@ -238,15 +203,15 @@ const Bridge = () => {
                                 <strong className='strong' onClick={() => setMax()}>MAX</strong>
                             </div>
                             <div className="input-select flex-row cb ac">
-                                <TokenImg url={fromData.token!} className={ 'visable-top-img'} name={fromData.token} width={20} height={20} marginRight={3} />
+                                <TokenImg url={fromData.token!} className={'visable-top-img'} name={fromData.token} width={20} height={20} marginRight={3} />
                                 <span>{fromData.token}</span>
                             </div>
                         </div>
                     </div>
-                    <h6 className="sub-tit mb0import mt15">
+                    <div className='mt15 flex-row cb ac'>
+                        <span>{fromData.network}</span>
                         <span>total: {fromData.balance} Kas</span>
-                        <span>{ fromData.network }</span>
-                    </h6>
+                    </div>
                 </div>
                 <div className='bridge-divider flex-row cc ac'>
                     <div className='bridge-icon' onClick={() => switchInfo()}>
@@ -271,14 +236,28 @@ const Bridge = () => {
                             style={{ fontSize: '14px', color: 'white', flex: 2 }}
                         />
                         <div className="input-select flex-row cb ac" >
-                            <TokenImg url={toData.token!} className={ 'visable-top-img'} name={toData.token} width={20} height={20} marginRight={3} />
+                            <TokenImg url={toData.token!} className={'visable-top-img'} name={toData.token} width={20} height={20} marginRight={3} />
                             <span>{toData.token}</span>
                         </div>
                     </div>
-                    <h6 className="sub-tit mb0import mt15">
-                        <span>{ toData.network }</span>
-                    </h6>
+                    <div className='mt15 flex-row cb ac'>
+                        <span>{toData.network}</span>
+                        <span></span>
+                    </div>
                 </div>
+                {
+                    toData.address && toData.address.includes('kaspa') ? (
+                        <div className='mt15'>
+                            <h6 className='sub-tit'>
+                                Recipient
+                            </h6>
+                            <div className="text-area">
+                                <textarea placeholder="Please Address" rows={3} value={l1Address || toData.address}
+                                    onChange={(e) => setL1Address(e.target.value)} />
+                            </div>
+                        </div>
+                    ) : null
+                }
                 <div className="btn-pos-two flexd-row post-bottom">
                     <Button block size="large" color="primary" loading={swapLoading} disabled={submitDisabled} onClick={() => swapSubmit()}>
                         Bridge
