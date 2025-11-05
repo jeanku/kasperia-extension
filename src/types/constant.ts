d6ca144373fbd226e5488f5a65529bf2b5a40b72
jemes <490573621@qq.com>
2025-11-05 17:37:49 +0800

    bridge function

--- src/types/constant.ts
+++ src/types/constant.ts
@@ -6,5 +6,5 @@ export const KasplexL2TestnetChainId = 167012
 export const KasplexL2MainnetChainId = 202555
 export const KasplexL1ToL2BridgeAddressForTestnet = "kaspatest:qyp7xxc2c2u0rs6uhgrs88ljjd0tlgjjxnu5a48899xmma894p68mggzct64wuu"
 export const KasplexL2ToL1BridgeAddressForTestnet = "0x6181d079fe60b44077e7a461d31519a53124fd54"
-export const KasplexL1ToL2BridgeAddressForMainnet = ""
-export const KasplexL2ToL1BridgeAddressForMainnet = ""
+export const KasplexL1ToL2BridgeAddressForMainnet = "kaspa:qypr0qj7luv26laqlquan9n2zu7wyen87fkdw3kx3kd69ymyw3tj4tsh467xzf2"
+export const KasplexL2ToL1BridgeAddressForMainnet = "0x34606e6d01280f49791628b311cf33a808d1f7c6"
